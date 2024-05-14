79ccbfd8a863edda13c5de4259ca741c41d10f8d
Maanas Nukala <manukala6@gmail.com>
2024-05-13 21:07:59 -0700

    Read in CSV

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+
+use chrono::prelude::*;
+use polars::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    let occurences_df = CsvReader::from_path("data\\OBIS_anemone_occurrences_slim.tsv")
+    .unwrap()
+    .infer_schema(None)
+    .has_header(true)
+    .with_separator(b'\t')
+    .finish()
+    .unwrap();
+
+    println!("Dataframe of OBIS occurence points: ");
+    println!("{}", occurences_df);
+
+    
 }
